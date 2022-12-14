5d183dc1b052de97adf059740e19fcef6cdd4105
paulohsdev <paulohsnutri@gmail.com>
2022-12-13 23:13:16 -0300

    atualização

--- IFTM/Atividades/Atividades1/Ex01.c
+++ IFTM/Atividades/Atividades1/Ex01.c
@@ -21,38 +21,39 @@ int main()
 
     media = ((laboratorio * 2) + (avaliacao * 3) + (exame * 5)) / 10;
 
-    printf("A média é igual a:", media);
+    // printf("A média é igual a: %.2f", media);
 
     if (media >= 0 && media <= 4.9)
     {
 
-        printf("A média é igual a:", media);
+        printf("A média é igual a: %.2f", media);
         printf("O conceito do aluno foi: E");
     }
     else if (media >= 5 && media <= 5.9)
     {
-        printf("A média é igual a:", media);
+        printf("A média é igual a: %.2f", media);
         printf("O conceito do aluno foi: D");
     }
     else if (media >= 6 && media <= 6.9)
     {
-        printf("A média é igual a:", media);
+        printf("A média é igual a: %.2f", media);
         printf("O conceito do aluno foi: C");
     }
     else if (media >= 6 && media <= 6.9)
     {
-        printf("A média é igual a:", media);
+        printf("A média é igual a: %.2f", media);
         printf("O conceito do aluno foi: C");
     }
     else if (media >= 7 && media <= 7.9)
     {
-        printf("A média é igual a:", media);
+        printf("A média é igual a: %.2f", media);
         printf("O conceito do aluno foi: B");
     }
     else if (media >= 8 && media <= 10)
     {
-        printf("A média é igual a:", media);
+        printf("A média é igual a: %.2f", media);
         printf("O conceito do aluno foi: A");
     }
+
     return 0;
 }
